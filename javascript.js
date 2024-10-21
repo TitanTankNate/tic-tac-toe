ba494dc6209de086e541a75338dc1152e7a1d9ce
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-10-21 16:00:32 -0400

    Create initial functions/methods
    + Created Player, ComputerPlayer, Board, BoardSquare, Board,
    and Game objects

--- javascript.js
+++ javascript.js
@@ -1,4 +1,239 @@
-console.log("Test");
+// FACTORY FUNCTION: Player
+// Description: 
+const Player = (name, score, lastMove) => {
+    // Setters
+    const setName = (input) => {name = input};
+    const setScore = (input) => {score = input};
+    const setLastMove = (input) => {lastMove = input};
+
+    // Getters
+    const getName = () => {return name};
+    const getScore = () => {return score};
+    const getLastMove = () => {return lastMove};
+    const getPlayerDetails = () => {
+        return {name, score, lastMove};
+    };
+
+    // Returns
+    return {
+        setName, getName,           // 'name' methods
+        setScore, getScore,         // 'score' methods
+        setLastMove, getLastMove,    // 'move' methods
+        getPlayerDetails
+    };
+}
+
+
+
+// FACTORY FUNCTION: ComputerPlayer
+// Description:
+const ComputerPlayer = (name, score, lastMove, difficulty) => {
+    // Setters
+    const setName = (input) => {name = input};
+    const setScore = (input) => {score = input};
+    const setLastMove = (input) => {lastMove = input};
+    const setDifficulty = (input) => {difficulty = input};
+
+    // Getters
+    const getName = () => {return name};
+    const getScore = () => {return score};
+    const getLastMove = () => {return lastMove};
+    const getCPUDetails = () => {
+        return {name, score, lastMove};
+    };
+    const getDifficulty = () => {return difficulty};
+
+    // Methods
+    const pickMove = (array, lastPlayerMove, gameWasReset) => {
+        // Private functions
+        // FUNCTION: weightedRandomNum
+        // Description:
+        // Attribution: https://stackoverflow.com/questions/8435183/generate-a-weighted-random-number
+        function weightedRandomNum(options) {
+            let i;
+            let sum=0;
+            numberChosen=Math.random();
+            for (i in options) {
+              sum += options[i];
+              if (numberChosen <= sum) return i;
+            };
+          };
+          
+        // Variables
+        let chosenMoveElementID;
+
+        // If no squares are occupied (game start/reset), pick
+        // a random square to start in.
+        if (gameWasReset) {
+            console.log("CPU will pick a square for the new game.");
+            // At lowest difficulty, CPU should select the middle 
+            // square, ID: 5.  At highest, it should pick a corner.
+            switch(difficulty) {
+                // On "easy" difficulty, the CPU is likely to make the
+                // mistake of choosing the center square, with all 
+                // other squares equally less likely.
+                case 0:
+                    chosenMoveElementID = weightedRandomNum({
+                        1:0.03, 2:0.03, 3:0.03,
+                        4:0.03, 5:0.76, 6:0.03, 
+                        7:0.03, 8:0.03, 9:0.03
+                    });
+                    break;
+                // On "medium" difficulty, the CPU will truly move 
+                // "randomly" for fairness.
+                case 1:
+                    chosenMoveElementID = Math.ceil(Math.random()*9);
+                    break;
+                // On "hard" difficulty, the CPU is most likely to 
+                // pick the corners, but might still select a side or
+                // the center square.
+                case 2:
+                    chosenMoveElementID = weightedRandomNum({
+                        1:0.16, 2:0.08, 3:0.16,
+                        4:0.08, 5:0.04, 6:0.08, 
+                        7:0.16, 8:0.08, 9:0.16
+                    });
+                    break;
+                // On "impossible" difficulty, the CPU will always
+                // unfairly pick a corner square.  The only thing
+                // random is the choice of corner.
+                case 3:
+                    chosenMoveElementID = weightedRandomNum({
+                        1:0.25, 2:0.00, 3:0.25,
+                        4:0.00, 5:0.00, 6:0.00, 
+                        7:0.25, 8:0.00, 9:0.25
+                    });
+                    break;
+            };
+
+            // console.log(chosenMoveElementID);
+            return chosenMoveElementID;
+        };
+    };
+
+    // Returns
+    return {
+        setName, getName,           // 'name' methods
+        setScore, getScore,         // 'score' methods
+        setLastMove, getLastMove,   // 'move' methods
+        getCPUDetails,
+        setDifficulty, getDifficulty,
+        pickMove
+    };
+}
+
+
+
+// FACTORY FUNCTION: BoardSquare
+// Description: 
+const BoardSquare = (elementID, row, col, isOccupied) => {
+    // Setters
+    
+    // Getters
+
+    // Methods
+
+    // Returns
+    return {
+        elementID, row, col, isOccupied};
+};
+
+
+
+// FACTORY FUNCTION: Board
+// Description: 
+const Board = () => {
+    // Private variables
+    let boardArray = [];
+
+    // Setters
+
+    // Getters
+    const getBoardArray = () => {return boardArray};
+
+    // Methods
+    const createBoard = () => {
+        // Private variables
+        let elementIDtoAssign = 1;
+        let newBoardSquare;
+
+        for (let rowCount=1;rowCount <=3; rowCount++) {
+            for (let colCount=1;colCount<=3;colCount++) {
+                newBoardSquare = BoardSquare(elementIDtoAssign,rowCount,colCount, false);
+                boardArray.push(newBoardSquare);
+                elementIDtoAssign++;
+            };
+        };
+    };
+
+    const occupySquare = (occupyID) => {
+        // Select the correct element of the array by elementID
+        let element = boardArray[(occupyID - 1)];
+
+        // If a space is marked as unoccupied, occupy it
+        if (!element.isOccupied) {
+            element.isOccupied = true;
+        }
+    };
+
+
+    // Returns
+    return {getBoardArray, createBoard, occupySquare};
+};
+
+
+
+// FACTORY FUNCTION: Game
+// Description:
+const Game = () => {
+    // Private variables
+    let gameWasReset = true;
+    let continueGame = false;
+    let currentMove;
+    
+    const newBoard = Board();
+    const newPlayer = Player("Player", 0, "none");
+    const newCPU = ComputerPlayer("CPU", 0, "none", 1);
+    let currentPlayer = "CPU";
+    
+    // Methods
+    const newGame = () => {
+        // Create new gameboard
+        newBoard.createBoard();
+        // console.log(newBoard.getBoardArray());
+
+        // Initialize flags
+        continueGame = true;
+    };
+
+    const doMainLoop = () => {
+        // while (continueGame) {
+            // TURN SELECTOR / PLAYER SELECTOR
+            switch (currentPlayer) {
+                case "Player":
+                    console.log(`It's ${newPlayer.getName()}'s turn.`);
+                    break;
+                case "CPU":
+                    console.log(`It's ${newCPU.getName()}'s turn.`);
+                    currentMove = newCPU.pickMove(newBoard, "none", true);
+                    newCPU.setLastMove(currentMove);
+                    console.log("Last move: " + newCPU.getLastMove());
+                    break;
+            }
+        // }
+    };
+
+    // Returns
+    return {gameWasReset, newGame, doMainLoop};
+};
+
+const newGameInstance = Game();
+newGameInstance.newGame();
+newGameInstance.doMainLoop();
+
+
+
+
 
 // O R I G I N A L   W R I T E   U P
 // This is going to be a lot more complex than I thought.
@@ -42,7 +277,7 @@ console.log("Test");
 //  6. User ideally blocks 3-6-9 by occupying 6.
 //  7. CPU should realize it was blocked and that 4-5-6 is now a
 //  threatening win condition.  CPU occupies 4.
-//  8. At this stage, winning is impossibl.  User will select either
+//  8. At this stage, winning is impossible.  User will select either
 //  of the remaining squares.
 //  9. CPU will take the last square, triggering a "all squares full"
 //  state, and ending the game in a draw.
