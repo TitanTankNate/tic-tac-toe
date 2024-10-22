7eafb30eb51c018d6e49dab2521e06dc893b5210
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-10-22 11:27:47 -0400

    Purge unneeded codebase and refactor
    Purged unneeded code, to more closely follow the TOP instructions

--- javascript.js
+++ javascript.js
@@ -25,110 +25,6 @@ const Player = (name, score, lastMove) => {
 
 
 
-// FACTORY FUNCTION: ComputerPlayer
-// Description:
-const ComputerPlayer = (name, score, lastMove, difficulty) => {
-    // Setters
-    const setName = (input) => {name = input};
-    const setScore = (input) => {score = input};
-    const setLastMove = (input) => {lastMove = input};
-    const setDifficulty = (input) => {difficulty = input};
-
-    // Getters
-    const getName = () => {return name};
-    const getScore = () => {return score};
-    const getLastMove = () => {return lastMove};
-    const getCPUDetails = () => {
-        return {name, score, lastMove};
-    };
-    const getDifficulty = () => {return difficulty};
-
-    // Methods
-    const pickMove = (array, lastPlayerMove, gameWasReset) => {
-        // Private functions
-        // FUNCTION: weightedRandomNum
-        // Description:
-        // Attribution: https://stackoverflow.com/questions/8435183/generate-a-weighted-random-number
-        function weightedRandomNum(options) {
-            let i;
-            let sum=0;
-            numberChosen=Math.random();
-            for (i in options) {
-              sum += options[i];
-              if (numberChosen <= sum) return i;
-            };
-          };
-          
-        // Variables
-        let chosenMoveElementID;
-
-        // If no squares are occupied (game start/reset), pick
-        // a random square to start in.
-        if (gameWasReset) {
-            console.log("CPU will pick a square for the new game.");
-            // At lowest difficulty, CPU should select the middle 
-            // square, ID: 5.  At highest, it should pick a corner.
-            switch(difficulty) {
-                // On "easy" difficulty, the CPU is likely to make the
-                // mistake of choosing the center square, with all 
-                // other squares equally less likely.
-                case 0:
-                    chosenMoveElementID = weightedRandomNum({
-                        1:0.03, 2:0.03, 3:0.03,
-                        4:0.03, 5:0.76, 6:0.03, 
-                        7:0.03, 8:0.03, 9:0.03
-                    });
-                    break;
-                // On "medium" difficulty, the CPU will truly move 
-                // "randomly" for fairness.
-                case 1:
-                    chosenMoveElementID = Math.ceil(Math.random()*9);
-                    break;
-                // On "hard" difficulty, the CPU is most likely to 
-                // pick the corners, but might still select a side or
-                // the center square.
-                case 2:
-                    chosenMoveElementID = weightedRandomNum({
-                        1:0.16, 2:0.08, 3:0.16,
-                        4:0.08, 5:0.04, 6:0.08, 
-                        7:0.16, 8:0.08, 9:0.16
-                    });
-                    break;
-                // On "impossible" difficulty, the CPU will always
-                // unfairly pick a corner square.  The only thing
-                // random is the choice of corner.
-                case 3:
-                    chosenMoveElementID = weightedRandomNum({
-                        1:0.25, 2:0.00, 3:0.25,
-                        4:0.00, 5:0.00, 6:0.00, 
-                        7:0.25, 8:0.00, 9:0.25
-                    });
-                    break;
-            };
-
-            // console.log(chosenMoveElementID);
-            return chosenMoveElementID;
-        } else {
-            console.log("CPU will pick the next square.");
-            console.log("Player last moved into square " + lastPlayerMove);
-            // Evaluate all adjacent squares for potential win conditions
-
-        };
-    };
-
-    // Returns
-    return {
-        setName, getName,           // 'name' methods
-        setScore, getScore,         // 'score' methods
-        setLastMove, getLastMove,   // 'move' methods
-        getCPUDetails,
-        setDifficulty, getDifficulty,
-        pickMove
-    };
-}
-
-
-
 // FACTORY FUNCTION: BoardSquare
 // Description: 
 const BoardSquare = (elementID, row, col, isOccupied) => {
@@ -186,15 +82,11 @@ const Board = () => {
         };
     };
 
-    const checkSurroundingSquares = (elementIDtoCheck) => {
-
-    };
-
     // Returns
     return {
         boardQuerySelector, 
         getBoardArray, getBoardSquareIsOccupied,
-        createBoard, occupySquare, checkSurroundingSquares
+        createBoard, occupySquare
     };
 };
 
@@ -211,7 +103,6 @@ const Game = () => {
     // Consctructors
     const newBoard = Board();
     const newPlayer = Player("Player", 0, "none");
-    const newCPU = ComputerPlayer("CPU", 0, "none", 1);
     
     // Methods
     const newGame = () => {
@@ -222,48 +113,18 @@ const Game = () => {
 
     const doMainLoop = (player) => {
         switch (player) {
-            case "Player":
+            case "player1":
                 // Do something with visuals
                 break;
-            case "CPU":
-                console.log(`It's ${newCPU.getName()}'s turn.`);
-                if (gameWasReset) {
-                    console.log("Game was reset.");
-                    currentMove = newCPU.pickMove(newBoard.getBoardArray(), "none", true);
-                    gameWasReset = false;
-                } else {
-                    console.log("Game was not reset.");
-                    currentMove = newCPU.pickMove(newBoard.getBoardArray(), newPlayer.getLastMove(), false);
-                };
+            case "player2":
 
-                newCPU.setLastMove(currentMove);
-                newBoard.occupySquare(currentMove);
-                console.log("Last move: " + newCPU.getLastMove());
-                currentPlayer = "Player";
-                console.log(`It's ${newPlayer.getName()}'s turn.`);
                 break;
         };
     };
 
     // Event handlers
     newBoard.boardQuerySelector.addEventListener("click", (event) => {
-        // If current player is the human, pick a square, otherwise 
-        // do nothing.
-        if (currentPlayer == "Player") {
-            // If selected square is occupied, inform user and do nothing
-            if(!newBoard.getBoardSquareIsOccupied(event.target.id)) {
-                console.log("Selected square " + event.target.id);
-                newPlayer.setLastMove(event.target.id);
-                currentPlayer = "CPU";
-                newGameInstance.doMainLoop("CPU");
-            } else {
-                console.log("That square is already occupied.");
-                newGameInstance.doMainLoop("Player");
-            };    
 
-        } else {
-            console.log("Not your turn.");
-        }
     });
 
     // Returns
